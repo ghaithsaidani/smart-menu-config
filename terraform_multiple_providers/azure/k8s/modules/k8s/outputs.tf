f63d83d6fadf081111394762a3982f02a0dc25b1
ghaithsaidani <ghaith.saidani@sesame.com.tn>
2024-11-13 00:35:54 +0100

    fix: explanation of needed variables

--- terraform_multiple_providers/azure/k8s/modules/k8s/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "principal_id" {
-  value = azurerm_kubernetes_cluster.aks.identity[0].principal_id
-}
-
-output "aks_cluster_id" {
-  value = azurerm_kubernetes_cluster.aks.id
-}
--- terraform_multiple_providers/azure/k8s/modules/resource_groups/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-  description = "resourece group name"
-  type        = string
-}
-
-variable "location" {
-  description = "resource group location"
-  type        = string
-}
